7a6cb71896454327bb486cdd2caaf500b48f6319
Ba Toan Nguyen <michael.batoan.nguyen@gmail.com>
2025-06-22 13:49:01 +1000

    Reconfig prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import peerDepsExternal from '@chrisneedham/rollup-plugin-peer-deps-external';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { resolve } from 'path';
+/// <reference types="vitest" />
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [peerDepsExternal(), nodeResolve(), dts({ insertTypesEntry: true })],
-  build: {
-    target: 'esnext',
-    lib: {
-      entry: resolve(__dirname, './index.ts'),
-      name: 'frontend-server',
-      fileName: 'frontend-server',
-    },
-    rollupOptions: {
-      external: ['fs', 'fs/promises', 'path', 'crypto', 'url'],
-    },
-    sourcemap: true,
-    minify: true,
-  },
   resolve: {
     alias: {
-      find: '@frontend-server',
-      replacement: resolve(__dirname, 'src/'),
+      '@frontend-server': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
