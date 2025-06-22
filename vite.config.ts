20465d23f4a985dd0a3c07703cdfebc6ab9d31f7
Ba Toan Nguyen <michael.batoan.nguyen@gmail.com>
2025-06-22 17:20:43 +1000

    Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
-/// <reference types="vitest" />
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import peerDepsExternal from '@chrisneedham/rollup-plugin-peer-deps-external';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  // @ts-ignore nodeResolve type is not compatible with Vite
+  plugins: [peerDepsExternal(), nodeResolve(), dts({ insertTypesEntry: true })],
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: resolve(__dirname, './index.ts'),
+      name: 'frontend-server',
+      fileName: 'frontend-server',
+    },
+    rollupOptions: {
+      external: ['fs', 'fs/promises', 'path', 'crypto', 'url'],
+    },
+    sourcemap: true,
+    minify: true,
+  },
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
